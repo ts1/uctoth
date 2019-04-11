a74505f799e19c4400971efdd1fea19640c6c31b
Takeshi Sone <takeshi.sone@gmail.com>
2019-04-11 16:12:16 +0900

    minimax.c: improve move sorting

--- ext/src/bb_minimax.c
+++ ext/src/bb_minimax.c
@@ -7,6 +7,9 @@
 #include "cache.h"
 
 #define SORT_DEPTH 3
+#define SORT_2_DEPTH 8
+#define SORT_3_DEPTH 10
+#define SORT_4_DEPTH 13
 #define CACHE_DEPTH 0
 
 #define MAX_MOVES 32
@@ -33,6 +36,11 @@ typedef struct {
 static cache_t cache;
 
 static int n_nodes;
+
+static inline int endgame(bboard b)
+{
+    return bb_disc_diff(b) > 0 ? BB_INF : -BB_INF;
+}
 
 static int minimax(bboard b, int depth, int *move_ptr, int n_discs,
         int lbound, int ubound, int pass)
@@ -67,7 +75,7 @@ static int minimax(bboard b, int depth, int *move_ptr, int n_discs,
     }
     if (best_move < 0) {
         if (pass)
-            return bb_score(b) * SCORE_MULT;
+            return endgame(b);
         else
             return -minimax(bb_swap(b), depth, move_ptr, n_discs,
                     -ubound, -lbound, 1);
@@ -135,7 +143,7 @@ static int sorted_minimax(bboard b, int depth, int *move_ptr, int n_discs,
 
     if (!n_moves) {
         if (pass)
-            return bb_score(b) * SCORE_MULT;
+            return endgame(b);
         else
             return -sorted_minimax(bb_swap(b), depth, move_ptr, n_discs,
                     -ubound, -lbound, 1);
@@ -146,6 +154,15 @@ static int sorted_minimax(bboard b, int depth, int *move_ptr, int n_discs,
             moves[i]->value = BB_INF;
         else if (lbound > -BB_INF && ubound - lbound == 1)
             moves[i]->value = -bb_potential_mobility(moves[i]->b);
+        else if (depth >= SORT_4_DEPTH) 
+            moves[i]->value = -minimax(moves[i]->b, 3, 0, n_discs+1,
+                    -BB_INF, BB_INF, 0);
+        else if (depth >= SORT_3_DEPTH) 
+            moves[i]->value = -minimax(moves[i]->b, 2, 0, n_discs+1,
+                    -BB_INF, BB_INF, 0);
+        else if (depth >= SORT_2_DEPTH) 
+            moves[i]->value = -minimax(moves[i]->b, 1, 0, n_discs+1,
+                    -BB_INF, BB_INF, 0);
         else
             moves[i]->value = -bb_eval(moves[i]->b, n_discs+1);
     }
@@ -243,12 +260,13 @@ deepening(bboard b, int max_depth, int max_nodes, int *move_ptr, int n_discs)
                 value = -sorted_minimax(moves[i]->b, depth, 0, n_discs + 1,
                     -BB_INF, -max, 0);
             }
-            moves[i]->value = value;
             if (value > max) {
+                moves[i]->value = value;
                 bb_debug("=%d", value);
                 max = value;
                 best = moves[i];
-            }
+            } else
+                moves[i]->value = -BB_INF;
             if (max_nodes && n_nodes >= max_nodes) {
                 bb_debug("\n");
                 goto double_break;
@@ -268,9 +286,9 @@ double_break:
 int bb_minimax(bboard b, int max_depth, int max_nodes, int *move_ptr)
 {
     assert(max_depth > 0 || max_nodes > 0);
-    if (!max_depth)
-        max_depth = 60; /* fail safe */
     int n_discs = bm_count_bits(b.black | b.white);
+    if (!max_depth || max_depth > 64 - n_discs)
+        max_depth = 64 - n_discs;
     if (max_depth == 1)
         return minimax(b, max_depth, move_ptr, n_discs, -BB_INF, BB_INF, 0);
     else
